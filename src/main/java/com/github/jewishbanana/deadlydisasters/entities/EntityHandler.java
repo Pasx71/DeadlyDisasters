e4860153c77c83db6b5c731d0a9c3c55ead3bc33
Pasx71 <80799158+Pasx71@users.noreply.github.com>
2026-03-17 17:33:38 +0200

    Add files via upload

--- src/main/java/com/github/jewishbanana/deadlydisasters/entities/EntityHandler.java
+++ src/main/java/com/github/jewishbanana/deadlydisasters/entities/EntityHandler.java
@@ -22,36 +22,15 @@ import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.persistence.PersistentDataType;
 
 import com.github.jewishbanana.deadlydisasters.Main;
-import com.github.jewishbanana.deadlydisasters.entities.christmasentities.Elf;
-import com.github.jewishbanana.deadlydisasters.entities.christmasentities.Frosty;
-import com.github.jewishbanana.deadlydisasters.entities.christmasentities.Grinch;
-import com.github.jewishbanana.deadlydisasters.entities.christmasentities.Santa;
-import com.github.jewishbanana.deadlydisasters.entities.easterentities.EasterBunny;
-import com.github.jewishbanana.deadlydisasters.entities.easterentities.KillerChicken;
-import com.github.jewishbanana.deadlydisasters.entities.easterentities.RampagingGoat;
-import com.github.jewishbanana.deadlydisasters.entities.endstormentities.BabyEndTotem;
-import com.github.jewishbanana.deadlydisasters.entities.endstormentities.EndTotem;
-import com.github.jewishbanana.deadlydisasters.entities.endstormentities.EndWorm;
-import com.github.jewishbanana.deadlydisasters.entities.endstormentities.VoidArcher;
-import com.github.jewishbanana.deadlydisasters.entities.endstormentities.VoidGuardian;
-import com.github.jewishbanana.deadlydisasters.entities.endstormentities.VoidStalker;
 import com.github.jewishbanana.deadlydisasters.entities.halloweenentities.Ghoul;
 import com.github.jewishbanana.deadlydisasters.entities.halloweenentities.Psyco;
-import com.github.jewishbanana.deadlydisasters.entities.halloweenentities.PumpkinKing;
 import com.github.jewishbanana.deadlydisasters.entities.halloweenentities.Scarecrow;
 import com.github.jewishbanana.deadlydisasters.entities.halloweenentities.Vampire;
-import com.github.jewishbanana.deadlydisasters.entities.purgeentities.DarkMage;
-import com.github.jewishbanana.deadlydisasters.entities.purgeentities.PrimedCreeper;
-import com.github.jewishbanana.deadlydisasters.entities.purgeentities.ShadowLeech;
 import com.github.jewishbanana.deadlydisasters.entities.purgeentities.SkeletonKnight;
-import com.github.jewishbanana.deadlydisasters.entities.purgeentities.SwampBeast;
-import com.github.jewishbanana.deadlydisasters.entities.purgeentities.TunnellerZombie;
 import com.github.jewishbanana.deadlydisasters.entities.purgeentities.ZombieKnight;
 import com.github.jewishbanana.deadlydisasters.entities.sandstormentities.AncientMummy;
 import com.github.jewishbanana.deadlydisasters.entities.sandstormentities.AncientSkeleton;
 import com.github.jewishbanana.deadlydisasters.entities.soulstormentities.LostSoul;
-import com.github.jewishbanana.deadlydisasters.entities.soulstormentities.SoulReaper;
-import com.github.jewishbanana.deadlydisasters.entities.soulstormentities.TamedLostSoul;
 import com.github.jewishbanana.deadlydisasters.utils.RepeatingTask;
 import com.github.jewishbanana.deadlydisasters.utils.Utils;
 
@@ -92,24 +71,6 @@ public class EntityHandler {
 					break;
 				}
 		switch (species) {
-		case "babyendtotem":
-			addEntity(new BabyEndTotem((Mob) e, plugin, rand));
-			break;
-		case "endtotem":
-			addEntity(new EndTotem((Mob) e, plugin, rand));
-			break;
-		case "endworm":
-			addEntity(new EndWorm((Mob) e, plugin, rand));
-			break;
-		case "voidguardian":
-			addEntity(new VoidGuardian((Mob) e, plugin, rand));
-			break;
-		case "voidarcher":
-			addEntity(new VoidArcher((Mob) e, plugin, rand));
-			break;
-		case "voidstalker":
-			addEntity(new VoidStalker((Mob) e, plugin, rand));
-			break;
 		case "lostsoul":
 			addEntity(new LostSoul((Mob) e, plugin, rand));
 			break;
@@ -119,53 +80,11 @@ public class EntityHandler {
 		case "ancientmummy":
 			addEntity(new AncientMummy((Mob) e, plugin, rand));
 			break;
-		case "primedcreeper":
-			addEntity(new PrimedCreeper((Mob) e, plugin));
-			break;
-		case "tunnellerzombie":
-			addEntity(new TunnellerZombie((Zombie) e, null, plugin));
-			break;
 		case "skeletonknight":
 			addEntity(new SkeletonKnight((Skeleton) e, plugin));
 			break;
-		case "darkmage":
-			addEntity(new DarkMage((Mob) e, plugin));
-			break;
-		case "soulreaper":
-			addEntity(new SoulReaper((Mob) e, plugin, rand));
-			break;
-		case "tamedlostsoul":
-			addEntity(new TamedLostSoul((Mob) e, plugin, rand, null));
-			break;
-		case "swampbeast":
-			addEntity(new SwampBeast((Mob) e, plugin));
-			break;
 		case "zombieknight":
 			addEntity(new ZombieKnight((Mob) e, plugin));
-			break;
-		case "shadowleech":
-			addEntity(new ShadowLeech((Zombie) e, plugin, rand));
-			break;
-		case "elf":
-			addEntity(new Elf((Zombie) e, plugin, rand));
-			break;
-		case "frosty":
-			addEntity(new Frosty((Snowman) e, plugin, rand));
-			break;
-		case "grinch":
-			addEntity(new Grinch((Mob) e, plugin, rand));
-			break;
-		case "santa":
-			addEntity(new Santa((Zombie) e, plugin, rand));
-			break;
-		case "rampaginggoat":
-			addEntity(new RampagingGoat((Goat) e, plugin));
-			break;
-		case "easterbunny":
-			addEntity(new EasterBunny((Rabbit) e, plugin, rand));
-			break;
-		case "killerchicken":
-			addEntity(new KillerChicken((Zombie) e, plugin));
 			break;
 		case "scarecrow":
 			addEntity(new Scarecrow((Zombie) e, plugin, rand));
@@ -178,9 +97,6 @@ public class EntityHandler {
 			break;
 		case "psyco":
 			addEntity(new Psyco((Mob) e, plugin, rand));
-			break;
-		case "pumpkinking":
-			addEntity(new PumpkinKing((Mob) e, plugin, rand));
 			break;
 		default:
 		}
